3c361bb24a132c1be6a2190c81cb33d86762d379
Clément Verrier <verrierclementrog@gmail.com>
2022-06-24 21:09:59 +0200

    Start gradient descent

--- mlutils/algos/gradients.py
+++ mlutils/algos/gradients.py
@@ -3,7 +3,28 @@ import numpy as np
 from ..models import predictions
 
 
-def mse(X: np.ndarray, y: np.ndarray, w: np.ndarray, b: float):
+def gradient_descent(X: np.ndarray, y: np.ndarray, w_init: np.ndarray,
+                     b_init: float = None, cost_func: function, grad_func: function,
+                     alpha: float, n_iters: int = 1000):
+    if b_init is not None:
+        w = np.concatenate(([b_init], w_init))
+    else:
+        w = w_init.copy()
+
+    loss_hist = []
+
+    for i in range(n_iters):
+        grad = grad_func(X, y, w)
+
+        w -= alpha * grad
+
+        if i % 10 == 0:
+            # TODO: Incorporate bias term in `w` in cost function and
+            # also in predictions.
+            loss_hist.append(cost_func(X, y, w))
+
+
+def mse(X: np.ndarray, y: np.ndarray, w: np.ndarray, b: float = None):
     """Compute gradient of mean squared error.
 
     Parameters
@@ -14,7 +35,7 @@ def mse(X: np.ndarray, y: np.ndarray, w: np.ndarray, b: float):
         Output 1-D array with `m` labels.
     w : array_like
         Parameters, 1-D array with the same number of features `n`. 
-    b : float
+    b : float, optional
         Bias parameter.
 
     Returns
@@ -25,7 +46,11 @@ def mse(X: np.ndarray, y: np.ndarray, w: np.ndarray, b: float):
     m, n = X.shape
 
     X_full = np.column_stack((np.ones(m), X))
-    w_full = np.concatenate(([b], w))
+
+    if b is not None:
+        w_full = np.concatenate(([b], w))
+    else:
+        w_full = w.copy()
 
     gradient = 1/m * X_full.T @ (X_full @ w_full - y)
 
--- mlutils/models/predictions.py
+++ mlutils/models/predictions.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def linreg(X: np.ndarray, w: np.ndarray, b: np.ndarray):
+def linreg(X: np.ndarray, w: np.ndarray, b: np.ndarray = None):
     """Compute predictions based on multiple linear regression model.
 
     Parameters
@@ -10,7 +10,7 @@ def linreg(X: np.ndarray, w: np.ndarray, b: np.ndarray):
         Input 2-D array with `m` examples and `n` features.
     w : array_like
         Parameters, 1-D array with the same number of features `n`. 
-    b : float
+    b : float, optional
         Bias parameter.
     """
     return X @ w + b
